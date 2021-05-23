214c12870c850ff523fedc8084e73af50d54aadf
Kelly Kauffman <kellykauffman1210@gmail.com>
2021-05-22 20:36:36 -0700

    added new files for categories and blogging url

---
+++ mysite/blogging/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from django.urls import path
+from blogging.views import stub_view
+
+urlpatterns = [path('', stub_view, name="blog_index"),
+                path('posts/<int:post_id>/', stub_view, name="blog_detail")]
