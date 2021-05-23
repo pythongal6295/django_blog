8e8183f483a1b7e64f97c2ad1c35b2307161d582
Kelly Kauffman <kellykauffman1210@gmail.com>
2021-05-23 16:27:46 -0700

    fixed error with control bar on webpage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout', LogoutView.as_view(next_page='/'), name="logout")
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout")
 ]
