2a5ac9e37096e46beebc43c28387806c7b00e2a1
Kelly Kauffman <kellykauffman1210@gmail.com>
2021-05-23 17:43:26 -0700

    changed post and category admin so categories can be chosen from post

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
