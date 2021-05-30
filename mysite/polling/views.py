32a10897cf7b244f2518bd6ef771398e530d4ed3
Kelly Kauffman <kellykauffman1210@gmail.com>
2021-05-30 16:44:36 -0700

    updated polling views to class views

--- mysite/polling/views.py
+++ mysite/polling/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polling/list.html', context)
+
+class PollListView(ListView):
+    model = Poll
+    template_name = 'polling/list.html'
+
+
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polling/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
+        if request.POST.get("vote") == "Yes":
+            poll.score += 1
+        elif request.POST.get("vote") == "No":
+            poll.score -= 1
+        else:
+            raise Http404
+        poll.save()
+
+        context = {"object": poll}
+        return render(request, "polling/detail.html", context)
+
 
 def detail_view(request, poll_id):
     try:
@@ -19,8 +42,6 @@ def detail_view(request, poll_id):
             poll.score -= 1
         else:
             raise Http404
-        # else:
-        #     poll.score -= 1
         poll.save()
 
     context = {'poll': poll}
